e80b52bccbb91238d69c890e25ff1f048a30cd8f
KhalilBenMzaker <166843735+KhalilBenMzaker@users.noreply.github.com>
2024-11-04 21:43:10 +0100

    Update SupplierRepository.java

--- src/main/java/tn/esprit/devops_project/repositories/SupplierRepository.java
+++ src/main/java/tn/esprit/devops_project/repositories/SupplierRepository.java
@@ -1,9 +1,16 @@
 package tn.esprit.devops_project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import tn.esprit.devops_project.entities.Product;
 import tn.esprit.devops_project.entities.Supplier;
 
+import java.util.List;
+
+@Repository
 public interface SupplierRepository extends JpaRepository<Supplier, Long> {
 
+    @Query("SELECT s FROM Supplier s JOIN s.productStock ps WHERE key(ps) = :product")
+    List<Supplier> findSuppliersByProduct(Product product);
 }
